cf4214aa2bfde2eab4e515f263b004d6ea65c0ca
lyfewithcode <lyfewithcode@gmail.com>
2021-07-26 12:40:27 +0700

    Registration form (Front end: Mark up and styling)

---
+++ inc/register-form.php
@@ -0,0 +1,22 @@
+<section class="registration">
+    <div class="section-header center">
+        <h1>Regitration</h1>
+        <h6><a href="index.php">Home</a> &gt; <span>Reagister</span></h6>
+    </div>
+    <div class="container">
+        <div class="row">
+            <form method="post" id="register-form" action="register.php">
+            <input type="text" name="fname" id="fname" placeholder="Enter First Name">
+            <input type="text" name="lname" id="lname" placeholder="Enter Last Name">
+            <input type="text" name="username" id="username" placeholder="Choose a Username">
+            <input type="email" name="email" id="email" placeholder="Enter your email address">
+            <input type="password" name="password" id="password" placeholder="Enter your Password here">
+            <input type="password" name="confirm-password" id="confirm-password" placeholder="Confirm your Password here">
+            <textarea name="bio" id="bio" placeholder="Enter your Bio here (Optional)"></textarea>
+            <input type="submit" name="submit" id="submit" value="Register" class="primary-bg white">
+            
+            </form>
+        </div>
+    </div>
+
+</section>

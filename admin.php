97268206dc35a220841bdae0df71c4e33359097c
lyfewithcode <lyfewithcode@gmail.com>
2021-07-31 16:27:46 +0700

    Admin page

---
+++ admin.php
@@ -0,0 +1,30 @@
+<?php
+
+    session_start();
+
+    $adminSession = NULL;
+
+    if(isset($_SESSION['user'])) {
+        unset($_SESSION['user']);
+    }
+
+    if(isset($_SESSION['admin'])) {
+        $adminSession = $_SESSION['admin'];
+        include 'function.php';
+    }
+    
+    else {
+        $adminSession = NULL;
+    }
+
+    include 'inc/header.php';
+    include 'inc/slider.php';
+
+    if($adminSession == NULL) {
+        include 'inc/admin-login-form.php';
+    }
+
+    else {
+        include 'inc/admin-content.php';
+    }
+?>

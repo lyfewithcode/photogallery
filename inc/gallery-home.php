6e0263676c98bfd78d6132baee835c9d2a7ad0b0
lyfewithcode <lyfewithcode@gmail.com>
2021-08-02 16:43:18 +0700

    Index page: Gallery section

--- inc/gallery-home.php
+++ inc/gallery-home.php
@@ -1,3 +1,14 @@
-<section class="gallery">
+<?php
+    include './function.php';
+?>
 
+<section class="gallery-content">
+    <div class="section-header center">
+        <h2>Gallery Images</h2>
+    </div>
+    <div class="container">
+        <div class="row">
+            <?php get_home_gallery_content(6); ?>
+        </div>
+    </div>
 </section>
